11e72f80f839c020aed2ca201b4149c339dfa610
OmarOudeh <omar.oudeh@saphirgmbh.de>
2021-09-21 17:49:18 +0200

    first commit

---
+++ src/main/java/com/example/testrelease/TestreleaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.testrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestreleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestreleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testrelease/TestreleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testrelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestreleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
